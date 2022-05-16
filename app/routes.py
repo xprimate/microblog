188e03c528059413d42d3143e4ead1028c228650
Jude Ogbonna <ogbonnajay@gmail.com>
2022-05-16 16:47:31 -0400

    Added posts with templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,23 @@
+from flask import render_template
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Iren'},
+            'body': 'Just another Lazy day in the South'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
