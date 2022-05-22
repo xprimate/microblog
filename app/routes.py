9ca1678099a467b1bc819562b61a53d77cc8df46
Jude Ogbonna <ogbonnajay@gmail.com>
2022-05-22 07:15:22 -0400

    database, migration, upgrade and setup

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Jude'}
     posts = [
         {
             'author': {'username': 'John'},
